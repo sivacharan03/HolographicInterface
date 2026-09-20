527bbd65864610ea1864e93670e551a04d13638b
sivacharan03 <sivacharan0364@gmail.com>
2026-09-20 16:11:14 +0530

    Auto update - 2026-09-20 16:11:14

--- main.py
+++ main.py
@@ -414,14 +414,7 @@ def distance(p1, p2):
         (p1.y - p2.y) ** 2
     )
 
-def is_point_inside_object(px, py, ox, oy, size):
-    half = size // 2
 
-    return (
-        ox - half <= px <= ox + half
-        and
-        oy - half <= py <= oy + half
-    )
 
 def main():
 
